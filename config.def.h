d4944a1f769fb4223f6cffd5dfaad4d5b2d71ee4
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-10-27 02:38:35 -0300

    set colors and lines

--- config.def.h
+++ config.def.h
@@ -8,13 +8,13 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*               fg         bg       */
+	[SchemeNorm] = { "#DDDDDD", "#2D2D2D" },
+	[SchemeSel]  = { "#2D2D2D", "#0099CC" },
+	[SchemeOut]  = { "#2D2D2D", "#23DE62" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
 /*
  * Characters not considered part of a word while deleting words
